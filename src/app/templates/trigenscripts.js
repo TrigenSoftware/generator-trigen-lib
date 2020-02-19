a1eb1552551d3132b6c7a7e9d1d6047680072129
dangreen <danon0404@gmail.com>
2020-02-19 21:57:59 +0700

    v2.1.0

--- src/app/templates/trigenscripts.js
+++ src/app/templates/trigenscripts.js
@@ -1,7 +1,7 @@
 import stringify from './stringify';
 
 export const name = '.trigenscriptsrc';
-export const scriptsVersion = '2';
+export const scriptsVersion = '4';
 
 export function scripts({
 	type,
